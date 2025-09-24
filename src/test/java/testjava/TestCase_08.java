0f0f621f5ff85712b9e878a8033547a2bf42e2c3
santu11-git <santubanerjee11@gmail.com>
2025-09-24 16:58:44 +0530

    This is my last updated commit. Feature:Folder wise screenshot comparison.

---
+++ src/test/java/testjava/TestCase_08.java
@@ -0,0 +1,16 @@
+package testjava;
+
+import VisualTest.VisualTest.VisualComparisonBatchFolderWiseUtil;
+import org.testng.annotations.Test;
+
+public class TestCase_08 {
+	
+	 @Test
+	    public void comparePolicyTypeScreenshots() {
+	        String baselinePath = "C:\\Users\\SREETOMA\\OneDrive\\Desktop\\VisualTest_Project\\VisualTest_Project\\src\\main\\resources\\BaselineMultipleImagesSS\\20250828_111703";
+	        String actualPath   = "C:\\Users\\SREETOMA\\OneDrive\\Desktop\\VisualTest_Project\\VisualTest_Project\\src\\main\\resources\\ActualMultipleImagesSS\\20250828_112025";
+
+	        VisualComparisonBatchFolderWiseUtil.compareUserDefinedFolders(baselinePath, actualPath);
+	    }
+
+}
