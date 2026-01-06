620397d5c3f6f4493b9804b5610a3471f90d2a52
santu11-git <santubanerjee11@gmail.com>
2026-01-07 01:05:20 +0530

    JAN 2026 Commit.

--- src/test/java/testjava/TestCase_08.java
+++ src/test/java/testjava/TestCase_08.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class TestCase_08 {
 	
 	 @Test
-	    public void comparePolicyTypeScreenshots() {
+	    public void comparePolicyTypeScreenshots() {  
 	        String baselinePath = "C:\\Users\\SREETOMA\\OneDrive\\Desktop\\VisualTest_Project\\VisualTest_Project\\src\\main\\resources\\BaselineMultipleImagesSS\\20250828_111703";
 	        String actualPath   = "C:\\Users\\SREETOMA\\OneDrive\\Desktop\\VisualTest_Project\\VisualTest_Project\\src\\main\\resources\\ActualMultipleImagesSS\\20250828_112025";
 
